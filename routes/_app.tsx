f3b30abe1b8ed3168fb3ec6dcfbf2a04b5a9b448
lojugt <lojugt5@gmail.com>
2026-05-23 15:36:41 -0400

    Optimize HTML lang and down-size favicon.ico fallback

--- routes/_app.tsx
+++ routes/_app.tsx
@@ -26,7 +26,7 @@ export default define.page(function App({ Component, state }) {
     : null;
 
   return (
-    <html>
+    <html lang="en">
       <head>
         <meta charset="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
